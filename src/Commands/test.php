b218468fa18652e4b2f24d3134907f1cb6153aab
Marcos Nunes <marcosnunesmbs@gmail.com>
2023-05-30 12:41:41 -0300

    command test

---
+++ src/Commands/test.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Wpcreator\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class test extends Command {
+
+    protected static $defaultName = 'test';
+
+    protected static $defaultDescription = 'Testando CLI WpCreator';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln("Teste feito com sucesso");
+
+        return Command::SUCCESS;
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit525230f3fa07fbdd97ffad3ec474577e
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit525230f3fa07fbdd97ffad3ec474577e', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit525230f3fa07fbdd97ffad3ec474577e', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit525230f3fa07fbdd97ffad3ec474577e
         call_user_func(\Composer\Autoload\ComposerStaticInit525230f3fa07fbdd97ffad3ec474577e::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit525230f3fa07fbdd97ffad3ec474577e::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
