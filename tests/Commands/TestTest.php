c5e18ac53fb57c430053611de7e53f86139afa8c
Marcos Nunes <marcosnunesmbs@gmail.com>
2023-08-04 21:15:43 -0300

    include autoload

--- tests/Commands/TestTest.php
+++ tests/Commands/TestTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Wpcreator\Tests\Commands;
-
 use App\Wpcreator\Commands\Test;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+
+require_once './vendor/autoload.php';
 
 class TestTest extends TestCase
 {
