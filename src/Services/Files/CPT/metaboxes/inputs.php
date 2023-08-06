7904c2501d2f12bec500c038875b1f0e9c246f3f
Marcos Nunes <marcosnunesmbs@gmail.com>
2023-08-06 12:05:31 -0300

    fix input get value

--- src/Services/Files/CPT/metaboxes/inputs.php
+++ src/Services/Files/CPT/metaboxes/inputs.php
@@ -1,3 +1,3 @@
     echo "
     <label for='__ID__'>__LABEL__</label>
-    <input type='__TYPE__' name='__ID__' id='__ID__' value='{$all_metas["__ID__"]}' style='width: 100%;'>";
+    <input type='__TYPE__' name='__ID__' id='__ID__' value='{$all_metas["__ID__"][0]}' style='width: 100%;'>";
